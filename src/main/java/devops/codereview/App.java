b4b9d67d3e5367a6aa81d26badbd19a200245d06
suhninlwin <40516571@live.napier.ac.uk>
2022-12-20 22:18:17 +0630

    List all countries in desc order

--- src/main/java/devops/codereview/App.java
+++ src/main/java/devops/codereview/App.java
@@ -1,10 +1,19 @@
 package devops.codereview;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class App
 {
-    public static void main(String[] args)
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
     {
         try
         {
@@ -17,9 +26,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -30,9 +37,6 @@ public class App
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "Team_4");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -45,7 +49,13 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -58,5 +68,72 @@ public class App
                 System.out.println("Error closing connection to database");
             }
         }
+    }
+
+    public ArrayList<Countries> getAllCountries()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT country.Name, country.Population FROM country ORDER BY country.Population DESC;";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Countries> countries = new ArrayList<Countries>();
+            while (rset.next())
+            {
+                Countries ctr = new Countries();
+                ctr.name = rset.getString("Name");
+                ctr.population = rset.getString("Population");
+                countries.add(ctr);
+            }
+            return countries;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details");
+            return null;
+        }
+    }
+
+    public void displayCountries(Countries ctr)
+    {
+        if (ctr != null)
+        {
+            System.out.println(ctr.name + " " + ctr.population + " ");
+        }
+    }
+
+    public void printCountries(ArrayList<Countries> countries)
+    {
+        // Print header
+        System.out.println(String.format("%-70s %-70s", "Name", "Population"));
+        // Loop over all employees in the list
+        for (Countries ctr : countries)
+        {
+            String ctr_string =
+                    String.format("%-70s %-70s",
+                            ctr.name, ctr.population);
+            System.out.println(ctr_string);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+        // Connect to database
+        a.connect();
+
+        // Display countries
+        ArrayList<Countries> countries = a.getAllCountries();
+        a.printCountries(countries);
+
+        // Disconnect from database
+        a.disconnect();
     }
 }
---
+++ src/main/java/devops/codereview/Countries.java
@@ -0,0 +1,9 @@
+package devops.codereview;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Countries {
+    public String name;
+    public String population;
+}
