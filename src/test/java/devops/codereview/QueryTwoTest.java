a425a5f149030388179b6f5ffea30693b6e0eaff
Saw Yu Nandar <Saw Yu Nandar@DESKTOP-KAE45PA.(none)>
2022-12-26 15:36:33 +0630

    Update

---
+++ src/test/java/devops/codereview/QueryTwoTest.java
@@ -0,0 +1,43 @@
+package devops.codereview;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class QueryTwoTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+
+    @Test
+    void printContinentTestEmpty()
+    {
+        ArrayList<Country> continent = new ArrayList<Country>();
+        app.printContinent(continent);
+    }
+
+    //print a list with a null value
+    @Test
+    void printContinentTestContainsNull()
+    {
+        ArrayList<Country> continent = new ArrayList<Country>();
+        continent.add(null);
+        app.printContinent(continent);
+    }
+    @Test
+    void printContinent()
+    {
+        ArrayList<Country> continent = new ArrayList<Country>();
+        Country cont = new Country();
+        cont.country_cont = "North America";
+        cont.country_population = "103000";
+        continent.add(cont);
+        app.printContinent(continent);
+    }
+}
